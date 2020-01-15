f700cb9294a33e6ca8f1eff7a645b73eb24600b4
faiz11amd <faiz11amd@gmail.com>
2020-01-15 13:14:01 +0530

    new

--- Angular7/NIT Project/ui/src/app/shared/directives/menu-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MenuClickDirective } from './menu-click.directive';
-
-describe('MenuClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MenuClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
